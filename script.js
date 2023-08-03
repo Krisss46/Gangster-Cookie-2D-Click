f93e26fe37fbacf6a0a87a754d9d976d71338cba
kriss46 <119404927+Krisss46@users.noreply.github.com>
2023-08-03 20:40:51 +0000

    Update script.js
    Bug fixes

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let cookieCount = parseInt(localStorage.getItem('cookieCount')) || 0;
 let cookieValue = parseInt(localStorage.getItem('cookieValue')) || 1;
-let upgradePrice = parseInt(localStorage.getItem('upgradePrice')) || 100;
-let upgradeLevels = [500, 3000, 10000, 25000];
+let upgradeLevels = [100, 500, 3000, 10000, 25000];
 let currentLevel = parseInt(localStorage.getItem('currentLevel')) || 0;
 
 const counterElement = document.getElementById('counter');
@@ -9,6 +8,8 @@ const cookieImgElement = document.getElementById('cookieImg');
 const cookieBtnElement = document.getElementById('cookieBtn');
 const resetBtnElement = document.getElementById('resetBtn');
 const priceElement = document.getElementById('price');
+
+let resetConfirmationTimeout;
 
 function addCookie() {
   cookieCount += cookieValue;
@@ -18,50 +19,63 @@ function addCookie() {
 
 function upgradeCookie() {
   if (currentLevel < upgradeLevels.length) {
-    if (cookieCount >= upgradePrice) {
-      cookieCount -= upgradePrice;
-      cookieValue *= 2;
-      upgradePrice = upgradeLevels[currentLevel];
-      currentLevel++;
-      counterElement.textContent = cookieCount;
-      localStorage.setItem('cookieCount', cookieCount);
-      localStorage.setItem('cookieValue', cookieValue);
-      localStorage.setItem('upgradePrice', upgradePrice);
-      localStorage.setItem('currentLevel', currentLevel);
-    }
+	if (cookieCount >= upgradeLevels[currentLevel]) {
+	  cookieCount -= upgradeLevels[currentLevel];
+	  cookieValue *= 2;
+	  currentLevel++;
+	  counterElement.textContent = cookieCount;
+	  priceElement.textContent = upgradeLevels[currentLevel];
+	  localStorage.setItem('cookieCount', cookieCount);
+	  localStorage.setItem('cookieValue', cookieValue);
+	  localStorage.setItem('currentLevel', currentLevel);
+	  updateUpgradeButton();
+	}
   } else {
-    cookieBtnElement.textContent = "Max level";
-    cookieBtnElement.disabled = true;
+	cookieBtnElement.textContent = "Max level";
+	cookieBtnElement.disabled = true;
   }
+}
 
-  updateUpgradeButton();
+function confirmReset() {
+  if (resetBtnElement.textContent === "Reset") {
+	resetBtnElement.textContent = "Sure?";
+	resetConfirmationTimeout = setTimeout(() => {
+	  resetBtnElement.textContent = "Reset";
+	}, 3000);
+  } else {
+	clearTimeout(resetConfirmationTimeout);
+	resetGame();
+  }
 }
 
 function resetGame() {
   localStorage.clear();
   cookieCount = 0;
   cookieValue = 1;
-  upgradePrice = 100;
   currentLevel = 0;
   counterElement.textContent = cookieCount;
+  priceElement.textContent = upgradeLevels[currentLevel];
+  cookieBtnElement.textContent = `Upgrade cookie for ${upgradeLevels[currentLevel]}`;
+resetBtnElement.textContent = "Reset";
+  cookieBtnElement.disabled = false;
   localStorage.setItem('cookieCount', cookieCount);
   localStorage.setItem('cookieValue', cookieValue);
-  localStorage.setItem('upgradePrice', upgradePrice);
   localStorage.setItem('currentLevel', currentLevel);
-  updateUpgradeButton();
 }
 
 function updateUpgradeButton() {
   if (currentLevel < upgradeLevels.length) {
-    cookieBtnElement.textContent = `Upgrade cookie for ${upgradePrice}`;
+	cookieBtnElement.textContent = `Upgrade cookie for ${upgradeLevels[currentLevel]}`;
   } else {
-    cookieBtnElement.textContent = "Max level";
-    cookieBtnElement.disabled = true;
+	cookieBtnElement.textContent = "Max level";
+	cookieBtnElement.disabled = true;
   }
 }
 
 cookieImgElement.onclick = addCookie;
+cookieBtnElement.onclick = upgradeCookie;
 
 // Set initial values on page load
 counterElement.textContent = cookieCount;
+priceElement.textContent = upgradeLevels[currentLevel];
 updateUpgradeButton();
